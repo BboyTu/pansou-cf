b9c320ce86a76f27c51693cf8af813925ec91ec1
BboyTu <156650959+BboyTu@users.noreply.github.com>
2026-09-07 02:16:17 +0800

    feat(v0.04): register 8 new plugins (10 total)

--- src/plugins/registry.ts
+++ src/plugins/registry.ts
@@ -5,8 +5,27 @@
 import type { SearchPlugin } from './types';
 import { apibay } from './apibay';
 import { pansearch } from './pansearch';
+import { quarkres } from './quarkres';
+import { ikantv } from './ikantv';
+import { meitizy } from './meitizy';
+import { hunhepan } from './hunhepan';
+import { quark4k } from './quark4k';
+import { ouge } from './ouge';
+import { cyg } from './cyg';
+import { nyaa } from './nyaa';
 
-export const PLUGINS: SearchPlugin[] = [apibay, pansearch];
+export const PLUGINS: SearchPlugin[] = [
+  apibay,
+  pansearch,
+  quarkres,
+  ikantv,
+  meitizy,
+  hunhepan,
+  quark4k,
+  ouge,
+  cyg,
+  nyaa,
+];
 
 /** 所有可用 channel 名 */
 export const CHANNELS = PLUGINS.map((p) => p.name);
